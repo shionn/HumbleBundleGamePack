356593ec40b0e4613304066773a7793c4763534b
Kepka Ludovic <shionn@gmail.com>
2025-09-13 08:55:53 +0200

    changement de la serialisation json

--- src/main/java/shionn/scanner/HumbleBundle.java
+++ src/main/java/shionn/scanner/HumbleBundle.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
-import org.json.JSONArray;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shionn.scanner.pojo.Bundle;
 
@@ -15,7 +15,7 @@ public class HumbleBundle {
 	public static void main(String[] args) throws InterruptedException, ParseException, IOException {
 		List<Bundle> bundles = new Scanner().doParsing();
 		try (FileWriter w = new FileWriter("data/game-bundles.json")) {
-			w.write(new JSONArray(bundles).toString(4));
+			new ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(w, bundles);
 		}
 	}
 
